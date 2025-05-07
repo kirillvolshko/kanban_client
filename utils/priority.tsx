556a0fc68881310e8cf3f7e465343dd02bebc10c
kyrylovoloshko <k.voloshko04@gmail.com>
2025-05-07 20:29:15 +0300

    fixed priority ux

--- utils/priority.tsx
+++ utils/priority.tsx
@@ -2,9 +2,9 @@ import { ChevronsDown, ChevronsUp, Equal } from "lucide-react";
 
 export const getPriority = (priority: "low" | "medium" | "high") => {
   const variants = {
-    low: <ChevronsUp size={20} className=" text-red-500" />,
+    low: <ChevronsDown size={20} className="text-green-600" />,
     medium: <Equal size={20} className=" text-blue-500" />,
-    high: <ChevronsDown size={20} className="text-green-600" />,
+    high: <ChevronsUp size={20} className=" text-red-500" />,
   };
   return variants[priority];
 };
